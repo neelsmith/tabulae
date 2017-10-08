f0aea328a793c9a7227b3e63586b50936ca15d0d
Neel Smith <dnsmith.neel@gmail.com>
2017-10-08 15:55:53 +0200

    Further scaffolding for build from tabular source

---
+++ project/BuildComposer.scala
@@ -0,0 +1,30 @@
+import sbt._
+import java.io.PrintWriter
+
+
+/** Object for composing all files that are generated from source,
+* rather than built from templates or data sets.  These include
+* the necessary makefiles, created by [[MakefileComposer]], and
+* the top-level FST files acceptor.fst (created by [[AcceptorComposer]]),
+* inflection.fst (created by [[InflectionComposer]]), and greek.fst
+* created by [[ParserComposer]].
+*/
+object BuildComposer {
+
+  def apply(repo: File, corpus: String, fstcompiler: String) : Unit = {
+    println("Composing a lot of build things.")
+    /*
+    val corpusDir = "parsers/" + corpus
+    val projectDir = repo / corpusDir
+    SymbolsComposer(repo, corpus)
+    IO.copyFile(repo / s"datasets/${corpus}/orthography/alphabet.fst",
+      repo / s"parsers/${corpus}/symbols/alphabet.fst")
+    InflectionComposer(projectDir)
+    AcceptorComposer(repo, corpus)
+    ParserComposer(projectDir)
+    MakefileComposer(projectDir, fstcompiler)
+    */
+    //GeneratorComposer(repo, corpus)
+  }
+
+}
---
+++ project/RulesInstaller.scala
@@ -0,0 +1,27 @@
+import sbt._
+import scala.io.Source
+import java.io.PrintWriter
+
+object RulesInstaller {
+
+
+  /** Format compilable FST rules for a named corpus.
+  *
+  * @param repo Base directory of the Kanónes repository.
+  * @param corpus Name of corpus
+  */
+  def apply(repo: File, corpus: String): Unit = {
+    println(s"\nConvert inflectional rules tables in ${repo} to FST")
+/*
+
+    val inflDir = DataInstaller.madeDir(repo / s"parsers/${corpus}/inflection")
+    val srcCorpus = repo / s"datasets/${corpus}"
+
+    NounRulesInstaller(srcCorpus / "rules-tables/nouns", inflDir / "nouninfl.fst")
+    IndeclRulesInstaller(srcCorpus / "rules-tables/indeclinable", inflDir / "indeclinfl.fst")
+    VerbRulesInstaller(srcCorpus / "rules-tables/verbs", inflDir / "verbinfl.fst")
+    */
+  }
+
+
+}
