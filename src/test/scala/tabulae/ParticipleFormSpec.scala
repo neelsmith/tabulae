f32f1389e63d2b05f74008401095ae245ef680dd
Neel Smith <dnsmith.neel@gmail.com>
2019-05-21 14:45:16 -0700

    Add placeholder for ParticipleFormSpec

---
+++ src/test/scala/tabulae/ParticipleFormSpec.scala
@@ -0,0 +1,12 @@
+
+package edu.holycross.shot.tabulae
+
+import org.scalatest.FlatSpec
+
+class ParticipleFormSpec extends FlatSpec {
+
+  "An AdjectiveForm" should "require GCND in constructor" in  pending
+
+  it should "implement all functions of the LemmatizedForm trait" in pending
+
+}
