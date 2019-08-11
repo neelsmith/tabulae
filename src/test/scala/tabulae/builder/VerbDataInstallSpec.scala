73b0d71ec7bd39c575672b26b5542242c119dc23
Neel Smith <dnsmith.neel@gmail.com>
2019-08-11 19:26:05 -0400

    Spel name reasonably

--- src/test/scala/tabulae/builder/VerbDataInstallSpec.scala
+++ src/test/scala/tabulae/builder/VerbDataInstallSpec.scala
@@ -6,17 +6,22 @@ import better.files._
 import java.io.{File => JFile}
 import better.files.Dsl._
 
+import java.util.Calendar
 
-// java.nio.file.DirectoryNotEmptyException: /Users/nsmith/Desktop/tabulae/src/test/resources/parsers
 
-class VerbDataInstallerSpec extends FlatSpec {
+
+class VerbDataInstallSpec extends FlatSpec {
+  val r = scala.util.Random
+  val millis = Calendar.getInstance().getTimeInMillis()
+  r.setSeed(millis)
+
 
 
   "The VerbDataInstaller object" should "install verb data from a single source" in {
     val datasets = File("src/test/resources/datasets/")
     val corpora = Vector("analytical_types")
-
-    val targetDir = File("src/test/resources/parsers/dummyparser/lexica")
+    val parserDir = File(s"src/test/resources/parsers/dummyparser-${r.nextInt(1000)}")
+    val targetDir = parserDir / "lexica"
     if (targetDir.exists) {
       File("src/test/resources/parsers").delete()
     }
@@ -24,8 +29,7 @@ class VerbDataInstallerSpec extends FlatSpec {
     assert(targetDir.exists,"VerbDataInstaller:  could not create " + targetDir)
 
     val targetFile = targetDir / "lexicon-verbs.fst"
-
-
+    println("Target " + targetFile)
 
     //dataSource: File, corpusList: Vector[String], targetFile: File
     VerbDataInstaller(datasets, corpora, targetFile)
@@ -36,6 +40,7 @@ class VerbDataInstallerSpec extends FlatSpec {
 
     try {
       File("src/test/resources/parsers").delete()
+      println("Deleted temp dir.")
     } catch {
       case t: Throwable => {
         println("Failed to delete parsers dir. " + t)
@@ -49,7 +54,8 @@ class VerbDataInstallerSpec extends FlatSpec {
   it should "do nothing if no verb data are present in a given corpus" in {
     val datasets = File("src/test/resources/datasets/")
     val corpora = Vector("no-lexica")
-    val targetDir = File("src/test/resources/parsers/dummyparser/lexica")
+    val parserDir = File("src/test/resources/parsers/dummyparser-${r.nextInt(1000)}")
+    val targetDir = parserDir / "lexica"
     val targetFile = targetDir / "lexicon-verbs.fst"
     if (targetDir.exists) {
       File("src/test/resources/parsers").delete()
@@ -76,27 +82,31 @@ class VerbDataInstallerSpec extends FlatSpec {
   it should "composite data from multiple sources" in {
     val datasets = File("src/test/resources/datasets/")
     val corpora = Vector("analytical_types", "shared")
-    val targetDir = File("src/test/resources/parsers/dummyparser/lexica")
+    val parserDir = File("src/test/resources/parsers/dummyparser-${r.nextInt(1000)}")
+    val targetDir = parserDir / "lexica"
     if (targetDir.exists) {
-      File("src/test/resources/parsers").delete()
+      val parserDir = File("src/test/resources/parsers")
+      parserDir.delete()
+      println("Removed " + parserDir  + ": " + (parserDir.exists == false))
     }
     mkdirs(targetDir)
+    assert(targetDir.exists, "VerbDataInstallerSpec: could not create " + targetDir)
     val targetFile = targetDir / "lexicon-verbs.fst"
     //dataSource: File, corpusList: Vector[String], targetFile: File
     VerbDataInstaller(datasets, corpora, targetFile)
-    assert(targetFile.exists)
+    assert(targetFile.exists, "VerbDataInstallerSpec: VerbDataInstaller did not create " + targetFile)
 
     val expectedLines = Vector("<u>proof\\.v1</u><u>lexent\\.n29616</u><#>monstr<verb><conj1>", "<u>proof\\.v2</u><u>lexent\\.n2280</u><#>am<verb><conj1>")
 
     assert(targetFile.lines.toVector.filter(_.nonEmpty) == expectedLines)
-    /*
+
     try {
-      File("src/test/resources/parsers").delete()
+    parserDir.delete()
     } catch {
       case t: Throwable => {
         println("Failed to delete parsers dir. " + t)
       }
-    }*/
+    }
 
   }
 
