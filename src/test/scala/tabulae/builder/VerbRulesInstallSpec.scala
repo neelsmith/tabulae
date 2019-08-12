26eed99117e41ab2ea7dd0858d704b5bae1b4f8e
Neel Smith <dnsmith.neel@gmail.com>
2019-08-12 06:01:47 -0400

    Test VerbRulesInstaller

--- src/test/scala/tabulae/builder/VerbRulesInstallSpec.scala
+++ src/test/scala/tabulae/builder/VerbRulesInstallSpec.scala
@@ -17,30 +17,30 @@ class VerbRulesInstallSpec extends FlatSpec {
   r.setSeed(millis)
 
 
-  "The VerbRulesInstaller object" should "install verb data from a single source" in pending /* {
+  "The VerbRulesInstaller object" should "install verb data from a single source" in   {
     val datasets = File("src/test/resources/datasets/")
     val corpora = Vector("analytical_types")
     val parserDir = File(s"src/test/resources/parsers/dummyparser-${r.nextInt(1000)}")
-    val targetDir = parserDir / "lexica"
+    val targetDir = parserDir / "inflection"
     if (targetDir.exists) {
       File("src/test/resources/parsers").delete()
     }
     mkdirs(targetDir)
-    assert(targetDir.exists,"VerbDataInstaller:  could not create " + targetDir)
+    assert(targetDir.exists,"VerbRulesInstaller:  could not create " + targetDir)
 
-    val targetFile = targetDir / "lexicon-verbs.fst"
+    val targetFile = targetDir / "verbinfl.fst"
     println("Target " + targetFile)
 
     //dataSource: File, corpusList: Vector[String], targetFile: File
-    VerbDataInstaller(datasets, corpora, targetFile)
-    assert(targetFile.exists,"VerbDataInstaller:  did not create " + targetFile)
+    VerbRulesInstaller(datasets, corpora, targetFile)
+    assert(targetFile.exists,"VerbRulesInstaller:  did not create destination file " + targetFile)
 
-    val expected = "<u>proof\\.v1</u><u>lexent\\.n29616</u><#>monstr<verb><conj1>"
-    assert(targetFile.lines.toVector(0) == expected)
+    val expectedLines = Vector("$verbinfl$ =  <conj1><verb>o<1st><sg><pres><indic><act><u>proof\\.are\\_presind1</u>", "$verbinfl$")
+    assert(targetFile.lines.toVector.filter(_.nonEmpty) == expectedLines)
 
     try {
       File("src/test/resources/parsers").delete()
-      println("Deleted temp dir.")
+
     } catch {
       case t: Throwable => {
         println("Failed to delete parsers dir. " + t)
@@ -49,12 +49,12 @@ class VerbRulesInstallSpec extends FlatSpec {
 
   }
 
-*/
+
 
   it should "do nothing if no verb data are present in a given corpus" in {
     val datasets = File("src/test/resources/datasets/")
     val corpora = Vector("no-lexica")
-    val parserDir = File("src/test/resources/parsers/dummyparser-${r.nextInt(1000)}")
+    val parserDir = File(s"src/test/resources/parsers/dummyparser-${r.nextInt(1000)}")
     val targetDir = parserDir / "lexica"
     val targetFile = targetDir / "lexicon-verbs.fst"
     if (targetDir.exists) {
@@ -64,25 +64,33 @@ class VerbRulesInstallSpec extends FlatSpec {
 
     VerbRulesInstaller(datasets, corpora, targetFile)
     assert(targetFile.exists == false)
+    try {
+      File("src/test/resources/parsers").delete()
+
+    } catch {
+      case t: Throwable => {
+        println("Failed to delete parsers dir. " + t)
+      }
+    }
   }
 
-  it should "return an empty string if no data are found in the source directory" in pending /*{
+  it should "return an empty string if no data are found in the source directory" in {
     val emptyDir = File("src/test/resources/no-fst")
-    val output = VerbDataInstaller.fstForVerbData(emptyDir)
+    val output = VerbRulesInstaller.fstForVerbRules(emptyDir)
     assert(output.isEmpty)
-  }*/
+  }
 
-  it should "return an empty string if asked to create FST strings from a non-existent directory" in pending /* {
+  it should "return an empty string if asked to create FST strings from a non-existent directory" in  {
     val emptyDir = File("src/test/resources/no-fst")
-    val fst = VerbDataInstaller.fstForVerbData(emptyDir)
+    val fst = VerbRulesInstaller.fstForVerbRules(emptyDir)
     assert(fst.isEmpty)
-  }*/
+  }
 
 
   it should "composite data from multiple sources" in pending /*{
     val datasets = File("src/test/resources/datasets/")
     val corpora = Vector("analytical_types", "shared")
-    val parserDir = File("src/test/resources/parsers/dummyparser-${r.nextInt(1000)}")
+    val parserDir = File(s"src/test/resources/parsers/dummyparser-${r.nextInt(1000)}")
     val targetDir = parserDir / "lexica"
     if (targetDir.exists) {
       val parserDir = File("src/test/resources/parsers")
@@ -90,11 +98,11 @@ class VerbRulesInstallSpec extends FlatSpec {
       println("Removed " + parserDir  + ": " + (parserDir.exists == false))
     }
     mkdirs(targetDir)
-    assert(targetDir.exists, "VerbDataInstallerSpec: could not create " + targetDir)
+    assert(targetDir.exists, "VerbRulesInstallerSpec: could not create " + targetDir)
     val targetFile = targetDir / "lexicon-verbs.fst"
     //dataSource: File, corpusList: Vector[String], targetFile: File
-    VerbDataInstaller(datasets, corpora, targetFile)
-    assert(targetFile.exists, "VerbDataInstallerSpec: VerbDataInstaller did not create " + targetFile)
+    VerbRulesInstaller(datasets, corpora, targetFile)
+    assert(targetFile.exists, "VerbRulesInstallerSpec: VerbRulesInstaller did not create " + targetFile)
 
     val expectedLines = Vector("<u>proof\\.v1</u><u>lexent\\.n29616</u><#>monstr<verb><conj1>", "<u>proof\\.v2</u><u>lexent\\.n2280</u><#>am<verb><conj1>")
 
