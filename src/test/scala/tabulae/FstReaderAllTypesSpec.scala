017b2b6fa58193430a63c878c9e26a0106a00bd2
Neel Smith <dnsmith.neel@gmail.com>
2019-08-18 20:48:01 -0400

    Unit test infinitive recognition

--- src/test/scala/tabulae/FstReaderAllTypesSpec.scala
+++ src/test/scala/tabulae/FstReaderAllTypesSpec.scala
@@ -20,6 +20,17 @@ class FstReaderAllTypesSpec extends FlatSpec {
     val infinFst = "> monstravisse\n<u>proof.v1</u><u>lexent.n29616</u><#>monstr<verb><conj1><div><conj1><infin>avisse<pft><act><u>proof.are_inf1</u>".split("\n").toVector
     println("\n\n\n")
     val parsed = FstReader.parseFstLines(infinFst)
+    val parse = parsed(0)
+    assert(parse.literalToken == "monstravisse")
+    assert(parse.analyses.size == 1)
+
+    val infForm: InfinitiveForm = parse.analyses(0) match {
+      case inf: InfinitiveForm => inf
+      case _ => fail("Nope, that wasn't an infinitive.")
+    }
+    assert(infForm.tense == Perfect)
+    assert(infForm.voice == Active)
+
   }
 
 
