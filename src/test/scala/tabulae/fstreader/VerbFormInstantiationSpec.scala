e7e7f7870563765d27a6ca435782c858f0d1e0db
Neel Smith <dnsmith.neel@gmail.com>
2019-08-19 05:19:50 -0400

    Define pending test for FstReader

---
+++ src/test/scala/tabulae/fstreader/VerbFormInstantiationSpec.scala
@@ -0,0 +1,25 @@
+
+package edu.holycross.shot.tabulae
+
+import org.scalatest.FlatSpec
+
+class VerbFormInstantiationSpec extends FlatSpec {
+
+
+  "The FstReader object" should  "instantiate regular conjugated verb forms from FST source" in pending /* {
+    val adverbFst = "> care\n<u>proof.adj1</u><u>lexent.n6903</u>car<adj><us_a_um><div><us_a_um><adv>e<pos><u>proof.us_a_um1</u>".split("\n").toVector
+    println("\n\n\n")
+    val parsed = FstReader.parseFstLines(adverbFst)
+    val parse = parsed(0)
+    assert(parse.literalToken == "care")
+    assert(parse.analyses.size == 1)
+
+    val infForm: AdverbForm = parse.analyses(0) match {
+      case adv: AdverbForm => adv
+      case _ => fail("Nope, that wasn't an adverb.")
+    }
+    assert(infForm.degree == Positive)
+  }*/
+
+
+}
