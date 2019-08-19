515bcb8f037232bd31ea03a2709551edee4e9d1e
Neel Smith <dnsmith.neel@gmail.com>
2019-08-19 09:11:11 -0400

    Fix typo in unit test

--- src/test/scala/tabulae/fstreader/ParticipleFormInstantiationSpec.scala
+++ src/test/scala/tabulae/fstreader/ParticipleFormInstantiationSpec.scala
@@ -8,7 +8,7 @@ class ParticipleFormInstantiationSpec extends FlatSpec {
 
   "The FstReader object" should  "instantiate regular participle forms from FST source" in  {
     val adverbFst = "> monstrans\n<u>proof.v1</u><u>lexent.n29616</u><#>monstr<verb><conj1><div><conj1><ptcpl>ans<masc><nom><sg><pres><act><u>proof.are_ptcpl1</u>".split("\n").toVector
-    println("\n\n\n")
+
     val parsed = FstReader.parseFstLines(adverbFst)
     val parse = parsed(0)
     assert(parse.literalToken == "monstrans")
@@ -18,9 +18,9 @@ class ParticipleFormInstantiationSpec extends FlatSpec {
       case ptcpl: ParticipleForm => ptcpl
       case _ => fail("Nope, that wasn't a participle.")
     }
-    assert(ptcplForm.gender == Positive)
-    assert(ptcplForm.grammaticalCase == Positive)
-    assert(ptcplForm.grammaticalNumber == Positive)
+    assert(ptcplForm.gender == Masculine)
+    assert(ptcplForm.grammaticalCase == Nominative)
+    assert(ptcplForm.grammaticalNumber == Singular)
   }
 
 
