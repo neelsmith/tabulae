71f813a08eecc436bc55eb9f16a5849853069417
Neel Smith <nsmith@Technites.local>
2020-07-25 03:52:14 -0400

    Add UrnManager class

---
+++ jvm/src/main/scala/tabulae/UrnManager.scala
@@ -0,0 +1,30 @@
+package edu.holycross.shot.tabulae
+
+import edu.holycross.shot.cite._
+import scala.io.Source
+
+case class UrnManager(collectionMap: Map[String, Cite2Urn]) {
+  def resolve(id: String): Option[Cite2Urn] = {
+    None
+  }
+}
+
+
+object UrnManager {
+
+  def apply(lines : Vector[String]) : UrnManager = {
+    val columns = lines.map(_.split("#")).toVector.tail
+    val idMap = Map.empty[String, Cite2Urn] ++ columns.map(row => (row(0) -> Cite2Urn(row(1))))
+    UrnManager (idMap)
+  }
+
+  def fromFile(f: String): UrnManager = {
+    val lines = Source.fromFile(f).getLines.toVector
+    UrnManager(lines)
+  }
+
+  def fromUrl(url: String): UrnManager = {
+    val lines = Source.fromURL(url).getLines.toVector
+    UrnManager(lines)
+  }
+}
---
+++ jvm/src/test/scala/tabulae/UrnManagerSpec.scala
@@ -0,0 +1,11 @@
+package edu.holycross.shot.tabulae
+
+import org.scalatest.FlatSpec
+
+
+class UrnManagerSpec extends FlatSpec {
+
+  "A UrnManager" should "expand IDs to URNs" in {
+    
+  }
+}
