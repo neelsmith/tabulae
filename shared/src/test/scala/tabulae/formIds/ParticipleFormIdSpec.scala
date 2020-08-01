2e7a9f6c95bbfa1aca7ab825ae45df8f24e13651
Neel Smith <dnsmith.neel@gmail.com>
2020-08-01 12:07:07 -0400

    Spec out ptcpl tests

--- shared/src/test/scala/tabulae/formIds/ParticipleFormIdSpec.scala
+++ shared/src/test/scala/tabulae/formIds/ParticipleFormIdSpec.scala
@@ -1,6 +1,8 @@
 
 package edu.holycross.shot.tabulae
 
+
+import edu.holycross.shot.cite._
 import org.scalatest.FlatSpec
 
 class ParticipleFormIdSpec extends FlatSpec {
@@ -9,5 +11,11 @@ class ParticipleFormIdSpec extends FlatSpec {
     val ptcplForm = ParticipleForm("ls.dummy", "ocremorph.test", "ocremorph.us_a_um42", Feminine, Nominative, Plural, Present, Active)
     val expected = "021012105"
     assert(ptcplForm.formId == expected)
+  }
+
+  it should "encode its form as a URN" in {
+    val ptcplForm = ParticipleForm("ls.dummy", "ocremorph.test", "ocremorph.us_a_um42", Feminine, Nominative, Plural, Present, Active)
+    val expected = Cite2Urn("urn:cite2:tabulae:morphforms.v1:021012105")
+    assert(ptcplForm.formUrn == expected)
   }
 }
--- shared/src/test/scala/tabulae/validforms/ValidParticipleSpec.scala
+++ shared/src/test/scala/tabulae/validforms/ValidParticipleSpec.scala
@@ -9,6 +9,8 @@ import org.scalatest.FlatSpec
 
 class ValidParticipleSpec extends FlatSpec {
 
+
+  //   val ptcpl = ValidForm(Cite2Urn("urn:cite2:tabulae:morphforms.v1:021012105"))
   "A ValidParticipleForm" should "accept forms with TVGCN for participle pos" in pending
 
 
